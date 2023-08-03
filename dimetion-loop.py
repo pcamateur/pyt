7b4912587a360a241fff5a340c815c2070ba299e
pcamateur <pcamateur@outlook.com>
2023-08-03 22:46:18 +0800

    loop edition2

--- dimetion-loop.py
+++ dimetion-loop.py
@@ -13,12 +13,17 @@ def calculate():
             for y in range(x, 200):
                 for z in range(y, 200):
                     dw = x * y * z * q / 6000
-                    if dw - t1 < 2 and t1 - dw < 2:
+                    # if dw - t1 < 2 and t1 - dw < 2:
+                    if t1 > dw and 0 < t1 - dw < 1:
                         lis.append([x, y, z])
 
     count(q, t1(d))
 
-    print(lis)
+    def sort_list(lst):
+        lst.sort(key=lambda x: abs(x[0] - x[1]) + abs(x[0] - x[2]) + abs(x[1] - x[2]))
+        return lst
+
+    print(sort_list(lis))
 
 while True:
     calculate()
